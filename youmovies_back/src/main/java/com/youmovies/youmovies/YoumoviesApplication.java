f74885335385fb35c156ef7445268f72592428c3
syoucef86 <100765624+syoucef86@users.noreply.github.com>
2023-02-22 21:33:15 +0100

    Add files via upload

---
+++ youmovies_back/src/main/java/com/youmovies/youmovies/YoumoviesApplication.java
@@ -0,0 +1,13 @@
+package com.youmovies.youmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoumoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoumoviesApplication.class, args);
+	}
+
+}
---
+++ youmovies_back/src/test/java/com/youmovies/youmovies/YoumoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youmovies.youmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoumoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
